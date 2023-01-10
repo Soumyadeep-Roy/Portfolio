8d00eac62b659d514c80fb108bccbeb8a449ae07
Soumyadeep Roy <74173003+Soumyadeep-Roy@users.noreply.github.com>
2023-01-10 11:32:30 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div className='footer_section'>
       <div className='src_code'>
-      Check out the source code here : <a href='https://github.com/Soumyadeep-Roy/Portfolio-Website' target="_blank">Link</a>
+      Check out the source code here : <a href='https://github.com/Soumyadeep-Roy/Portfolio' target="_blank">Link</a>
       </div>
       <div className='top'>
       Go to  <HashLink smooth to="/#home"><a>Top</a></HashLink>
